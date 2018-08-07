e79be3945de88f37605a91beae80ffaa63c180ae
bdelseny <bdelseny@bdelseny-debian.bdelseny.debian>
2018-08-07 22:23:02 +0200

    Add my first component in Vue.js

---
+++ front/src/components/Patate.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Patate',
+    props: {
+        msg: String
+    }
+}
+</script>
--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Patate msg="Test Patate"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Patate from '@/components/Patate.vue'
+
+export default {
+  name: 'about',
+  components: {
+    Patate
+  }
+}
+</script>
