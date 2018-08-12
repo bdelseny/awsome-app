9b492cd6f244b10241a2a3af010629d3be4922e4
bdelseny <bdelseny@bdelseny-debian.bdelseny.debian>
2018-08-12 15:28:57 +0200

    first call: need to display the response in front

--- front/src/components/Patate.vue
+++ front/src/components/Patate.vue
@@ -4,9 +4,20 @@
 
 <script>
 export default {
-    name: 'Patate',
-    props: {
-        msg: String
+  name: "Patate",
+  props: {},
+  computed: {
+    msg: function() {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "text/plain");
+      var getUsers = new Request("http://localhost:3000/users", {
+        method: "GET",
+        headers: myHeaders,
+        mode: "cors",
+        cache: "default"
+      });
+      fetch(getUsers).then(console.log);
     }
-}
+  }
+};
 </script>
