1a19ab972137a7fd7b5e4a84c35fe7224865638f
bdelseny <bdelseny@SlimPotato.potato.fr>
2018-08-17 18:55:18 +0200

    Response shown in console
    Need to figure how to add it async in interface

--- front/src/components/Patate.vue
+++ front/src/components/Patate.vue
@@ -6,8 +6,19 @@
 export default {
   name: "Patate",
   props: {},
-  computed: {
-    msg: function() {
+  created() {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.msg = this.fetchData();
+  },
+  watch: {
+    // call again the method if the route changes
+    $route: "fetchData"
+  },
+  methods: {
+    fetchData() {
+      this.msg = "Method called";
+      var that = this;
       var myHeaders = new Headers();
       myHeaders.append("Content-Type", "text/plain");
       var getUsers = new Request("http://localhost:3000/users", {
@@ -16,7 +27,23 @@ export default {
         mode: "cors",
         cache: "default"
       });
-      fetch(getUsers).then(console.log);
+      return fetch(getUsers)
+        .then(response => {
+          response.json().then(function(data) {
+            console.log(data.test);
+            return data.test;
+          });
+        })
+        .catch(console.log);
+      // replace `getPost` with your data fetching util / API wrapper
+      // getPost(this.$route.params.id, (err, post) => {
+      //   this.loading = false;
+      //   if (err) {
+      //     this.error = err.toString();
+      //   } else {
+      //     this.post = post;
+      //   }
+      // });
     }
   }
 };
